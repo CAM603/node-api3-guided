5c3a5a93fa1b55d26929fcb5100b6803dda916b7
CAM603 <cameronhawley@comcast.net>
2020-02-12 13:51:29 -0500

    finished lecture

--- server.js
+++ server.js
@@ -1,19 +1,48 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
+const morgan = require("morgan");
+const helmet = require("helmet")
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+function gateKeeper(guess) {
 
-server.use('/api/hubs', hubsRouter);
+  return function(req, res, next) {
+    const password = req.headers.password;
+  
+    if(password && password.toLowerCase() === guess) {
+      next()
+    } else {
+      res.status(401).json({ message: 'you shall not pass'})
+    }
+  }
+}
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+function greeter(req, res, next) {
+  req.cohort = "Web 26"
+  next()
+}
 
+function logger(req, res, next) {
+  console.log(`${req.method} request to ${req.originalUrl}`)
+
+  next()
+}
+
+// middleware
+server.use(express.json()); // built in middleware
+// server.use(morgan("dev"));
+server.use(helmet())
+// server.use(logger)
+
+// routes - endpoints
+server.use('/api/hubs', logger, gateKeeper('mellon'), hubsRouter);
+
+server.get('/', logger, greeter, gateKeeper('poop'), (req, res) => {
   res.send(`
     <h2>Lambda Hubs API</h2>
-    <p>Welcome${nameInsert} to the Lambda Hubs API</p>
+    <p>Welcome ${req.cohort} to the Lambda Hubs API</p>
     `);
 });
 
